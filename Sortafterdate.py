8685f3cbc49c87051cb3644d3def1c9f03526cae
Thomas Bratzdrum <Thomas.Bratzdrum@student.uni-augsburg.de>
2019-02-14 09:34:33 +0100

    Created initial sorter

---
+++ Sortafterdate.py
@@ -0,0 +1,29 @@
+import os
+from bs4 import BeautifulSoup as bs
+import numpy as np
+import shutil
+import time
+
+
+pfad = "D:\BTFV-Elo"
+dateien = os.listdir(pfad)
+i=0
+for element in dateien:
+    if element != "Datumssortiert":
+
+        # time.sleep(0.5)
+        datum_ger = element.split('_')[-1].strip('.html')
+        date = datum_ger.split('-')
+        datum = date[2]+ "-" + date[1] + "-" + date[0]
+        # print(datum)
+        newpfad = pfad + "\\" + "Datumssortiert"
+        # print(element)
+        if not os.path.exists(newpfad):
+            os.makedirs(newpfad)
+        if not os.path.exists(newpfad + '\\' + datum + '.html'):
+            shutil.copy(pfad + '\\' + element, newpfad + '\\' + datum + '.html')
+        else:
+            shutil.copy(pfad + '\\' + element, newpfad + '\\'+ datum + '(' + str(i) + ')' + '.html')
+        i += 1
+        print(i)
+        #
