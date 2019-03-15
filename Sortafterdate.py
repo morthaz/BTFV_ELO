20c6945e0204ec4f6597f8c53c0d59f983d6b5c7
morthaz <tom.bratzdrum@gmail.com>
2019-03-16 00:13:42 +0100

    replace empty class with class players in html file to recognize names

--- Sortafterdate.py
+++ Sortafterdate.py
@@ -5,9 +5,9 @@ import shutil
 Um eine Elo-Wertung zu erstellen sollten die Spielberichte chronologisch abgespeichert werden. 
 """
 
-pfad = "D:\BTFV-Elo"
+pfad = "G:\BTFV-Elo"
 dateien = os.listdir(pfad)
-i=0
+i = 0
 for element in dateien:
     if element != "Datumssortiert":
 
@@ -17,13 +17,19 @@ for element in dateien:
         datum = date[2]+ "-" + date[1] + "-" + date[0]
         # print(datum)
         newpfad = pfad + "\\" + "Datumssortiert"
+        pathin = pfad + '\\' + element
+        pathout = newpfad + '\\' + datum + '(' + str(i) + ')' + '.html'
         # print(element)
         if not os.path.exists(newpfad):
             os.makedirs(newpfad)
         if not os.path.exists(newpfad + '\\' + datum + '.html'):
             shutil.copy(pfad + '\\' + element, newpfad + '\\' + datum + '.html')
         else:
-            shutil.copy(pfad + '\\' + element, newpfad + '\\'+ datum + '(' + str(i) + ')' + '.html')
+            with open(pathin, 'rt') as fin:
+                with open(pathout, 'wt') as fout:
+                    for line in fin:
+                        fout.write(line.replace('class=\"\"', 'class=\"player\"'))
+            # shutil.copy(pfad + '\\' + element, newpfad + '\\'+ datum + '(' + str(i) + ')' + '.html')
         i += 1
         print(i)
         #
