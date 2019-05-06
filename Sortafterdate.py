e2653e415d6552318d30525240bcb0c71c2c8319
scipper15 <reinhard.eichhorn@gmail.com>
2019-05-06 05:14:31 +0200

    Removed debugging messages and renamed variable

--- Sortafterdate.py
+++ Sortafterdate.py
@@ -12,8 +12,8 @@ for subdir, dirs, files in os.walk(game_results_dir):
     for file in files:
         # time.sleep(0.5)
         datum_ger = file.split('_')[-1].strip('.html')
-        date = datum_ger.split('-')
-        datum = date[2]+ "-" + date[1] + "-" + date[0]
+        date_parts = datum_ger.split('-')
+        datum = date_parts[2]+ "-" + date_parts[1] + "-" + date_parts[0]
         # print(datum)
         new_path = base_path + "\\Datumssortiert"
         pathin = os.path.join(subdir, file)
@@ -26,9 +26,7 @@ for subdir, dirs, files in os.walk(game_results_dir):
             shutil.copy(subdir + "\\" + file, base_path  + "\\Datumssortiert\\" + datum + '.html')
         else:
             with open(pathin, 'rt') as fin:
-                print("open pathin rt")
                 with open(pathout, 'wt') as fout:
-                    print("open pathout wt")
                     for line in fin:
                         fout.write(line.replace('class=\"\"', 'class=\"player\"'))
             # shutil.copy(game_results_dir + '\\' + element, game_results_dir + '\\'+ datum + '(' + str(i) + ')' + '.html')
