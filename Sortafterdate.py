5d296fa84cbf503b617ee473cbc000e80ced9fb9
scipper15 <reinhard.eichhorn@gmail.com>
2019-05-06 05:12:05 +0200

    Sorting all files from BTFV-Spielergebnisse to Datumssortiert

--- Sortafterdate.py
+++ Sortafterdate.py
@@ -5,31 +5,32 @@ import shutil
 Um eine Elo-Wertung zu erstellen sollten die Spielberichte chronologisch abgespeichert werden. 
 """
 
-pfad = "G:\BTFV-Elo"
-dateien = os.listdir(pfad)
+base_path = os.path.dirname(os.path.abspath(__file__))
+game_results_dir = os.path.dirname(os.path.abspath(__file__)) + "\\BTFV-Spielergebnisse"
 i = 0
-for element in dateien:
-    if element != "Datumssortiert":
-
+for subdir, dirs, files in os.walk(game_results_dir):
+    for file in files:
         # time.sleep(0.5)
-        datum_ger = element.split('_')[-1].strip('.html')
+        datum_ger = file.split('_')[-1].strip('.html')
         date = datum_ger.split('-')
         datum = date[2]+ "-" + date[1] + "-" + date[0]
         # print(datum)
-        newpfad = pfad + "\\" + "Datumssortiert"
-        pathin = pfad + '\\' + element
-        pathout = newpfad + '\\' + datum + '(' + str(i) + ')' + '.html'
-        # print(element)
-        if not os.path.exists(newpfad):
-            os.makedirs(newpfad)
-        if not os.path.exists(newpfad + '\\' + datum + '.html'):
-            shutil.copy(pfad + '\\' + element, newpfad + '\\' + datum + '.html')
+        new_path = base_path + "\\Datumssortiert"
+        pathin = os.path.join(subdir, file)
+        pathout = new_path + '\\' + datum + '(' + str(i) + ')' + '.html'
+        print(pathin)
+        print(pathout)
+        if not os.path.exists(new_path):
+            os.makedirs(new_path)
+        if not os.path.exists(new_path + '\\' + datum + '.html'):
+            shutil.copy(subdir + "\\" + file, base_path  + "\\Datumssortiert\\" + datum + '.html')
         else:
             with open(pathin, 'rt') as fin:
+                print("open pathin rt")
                 with open(pathout, 'wt') as fout:
+                    print("open pathout wt")
                     for line in fin:
                         fout.write(line.replace('class=\"\"', 'class=\"player\"'))
-            # shutil.copy(pfad + '\\' + element, newpfad + '\\'+ datum + '(' + str(i) + ')' + '.html')
+            # shutil.copy(game_results_dir + '\\' + element, game_results_dir + '\\'+ datum + '(' + str(i) + ')' + '.html')
         i += 1
         print(i)
-        #
